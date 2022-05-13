6f2bf8533e5b70bdc0edf1f85d40065e70377484
Kth <vcro12123@naver.com>
2022-05-13 21:38:51 +0900

    ref로 dom 조작

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,45 +1,39 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 import Wrapper from '../Helpers/Wrapper';
 const AddUser = (props) => {
-  const [enteredUserName, setEnteredUserName] = useState('');
-  const [enteredUserAge, setEnteredUserAge] = useState('');
   const [error, setError] = useState();
-  const [isModal, setIsModal] = useState(false);
+  const nameInputRef = useRef(null);
+  const ageInputRef = useRef(null);
+
   const addUserHandler = (e) => {
     e.preventDefault();
-    if (
-      enteredUserName.trim().length === 0 ||
-      enteredUserAge.trim().length === 0
-    ) {
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'please enter a valid name and age(non-empty values)',
       });
-      setIsModal(!isModal);
+
       return;
     }
-    if (+enteredUserAge < 1) {
+    if (+enteredAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'please enter a valid age(>0)',
       });
-      setIsModal(!isModal);
+
       return;
     }
-    props.addUser(enteredUserName, enteredUserAge);
-    setEnteredUserName('');
-    setEnteredUserAge('');
+    props.addUser(enteredName, enteredAge);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
-  const userNameChangeHandler = (e) => {
-    setEnteredUserName(e.target.value);
-  };
-  const userAgeChangeHandler = (e) => {
-    setEnteredUserAge(e.target.value);
-  };
+
   const errorHandler = () => {
     setError(null);
   };
@@ -49,7 +43,6 @@ const AddUser = (props) => {
         <ErrorModal
           title={error.title}
           message={error.message}
-          onModal={setIsModal}
           onConfirm={errorHandler}
         />
       )}
@@ -57,19 +50,9 @@ const AddUser = (props) => {
       <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor='username'>UserName</label>
-          <input
-            type={'text'}
-            id='username'
-            value={enteredUserName}
-            onChange={userNameChangeHandler}
-          ></input>
+          <input type={'text'} id='username' ref={nameInputRef} />
           <label htmlFor='age'>UserAge</label>
-          <input
-            type={'number'}
-            id='age'
-            value={enteredUserAge}
-            onChange={userAgeChangeHandler}
-          ></input>
+          <input type={'number'} id='age' ref={ageInputRef} />
           <Button type='submit'>Add User</Button>
         </form>
       </Card>
